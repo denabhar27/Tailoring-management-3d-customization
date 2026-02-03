b3e11389bfa9d50da8f804016c1e16c721088dca
nedmamiala-prog <nedmamiala@gmail.com>
2026-02-03 10:39:08 +0800

    test for merge

--- backend/database/alter_rental_inventory_size.sql
+++
@@ -1,6 +0,0 @@
--- Alter rental_inventory table to change size column from VARCHAR(20) to TEXT
--- This allows storing JSON measurements data
-
-ALTER TABLE rental_inventory 
-MODIFY COLUMN size TEXT NULL;
-
--- backend/database/update_payment_status_enum.sql
+++
@@ -1,5 +0,0 @@
--- Update payment_status enum to include down-payment and fully_paid
-ALTER TABLE order_items 
-MODIFY COLUMN payment_status ENUM('unpaid', 'paid', 'cancelled', 'down-payment', 'fully_paid') 
-DEFAULT 'unpaid';
-
